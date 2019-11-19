b39c06cec4a86a953e58b78a360a6d3891e0f758
Cole Grigsby <cgrigsby@equinix.com>
2019-11-19 11:26:29 -0800

    Adding FilterSwap public files

---
+++ FilterSwap/filters.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    console.log("Starting Extension")
+    let filterParameter = "Multiple Filters Parameter"
+    let parameterValues = []; 
+    let currentParam; 
+    let parameterZones = {};
+
+
+    tableau.extensions.initializeAsync().then(function() {
+        tableau.extensions.dashboardContent.dashboard.objects.forEach(function(object){
+            console.log(object.name + ":" + object.type + ":" + object.id + ":" + object.isVisible);
+            parameterZones[object.name] = object.id;
+          });
+        tableau.extensions.dashboardContent.dashboard.findParameterAsync(filterParameter).then(function(parameter){
+            console.log(parameter);
+            currentParam = parameter.currentValue.formattedValue;
+            parameter.allowableValues.allowableValues.forEach(function(val){ parameterValues.push(val._formattedValue);})
+
+            parameterValues.forEach(function(val){
+                if (val != currentParam)
+                    hide(val);
+            })
+            show(currentParam);
+
+            parameter.addEventListener(tableau.TableauEventType.ParameterChanged, changeParameter)
+        })
+
+        })
+
+    function changeParameter(){
+        // clears all filters.... 
+        // tableau.dashboard.worksheets.forEach(function(worksheet) {
+        //     worksheet.getFiltersAsync().then(function(filtersForWorksheet) {
+        //         let filterClearPromises = [];
+        //         filtersForWorksheet.forEach(function(filter) {
+        //             filterClearPromises.push(worksheet.clearFilterAsync(filter.fieldName));
+        //         });
+        //     });
+        // });
+
+        tableau.extensions.dashboardContent.dashboard.findParameterAsync(filterParameter).then(function(parameter){
+            console.log(currentParam, parameter.currentValue.formattedValue);
+            hide(currentParam);
+            currentParam = parameter.currentValue.formattedValue;
+            show(currentParam);
+        })
+    }
+
+    function hide(zoneName){
+        let id = parameterZones[zoneName];
+        let hideDict = {};
+        hideDict[id] = tableau.ZoneVisibilityType.Hide;
+        tableau.extensions.dashboardContent.dashboard.setZoneVisibilityAsync(hideDict).then(function() {
+            console.log("hide done");
+        });
+    }
+
+    function show(zoneName){
+        let id = parameterZones[zoneName];
+        let showDict = {};
+        showDict[id] = tableau.ZoneVisibilityType.Show;
+        tableau.extensions.dashboardContent.dashboard.setZoneVisibilityAsync(showDict).then(function() {
+            console.log("show done");
+        });
+    }
+
+});
